ec1f6fbb31fe960e7b1e3dd4b8e909c6a01b5187
BEZYL MOPHAT OTIENO <96384227+Bezyl-Mophat-Otieno@users.noreply.github.com>
2023-08-20 15:11:13 +0300

    Add Product Controller tested successfully

--- src/controllers/product.controllers/addProduct.ts
+++ src/controllers/product.controllers/addProduct.ts
@@ -7,16 +7,19 @@ const addProduct = async (req: Request, res: Response) => {
   try {
     const id = v4();
     const { name, description, price } = req.body;
+
     if (!name || !description || !price)
       return res
         .status(StatusCodes.BAD_REQUEST)
         .json({ message: "Please fill all fields" });
     const result = await DB.executeProcedure("addProduct", { ...req.body, id });
+
     if (result.rowsAffected[0] === 0)
       return res.status(StatusCodes.BAD_REQUEST).json({
         message: "Something went wrong,The Product was not added",
         status: "failed",
       });
+
     return res
       .status(StatusCodes.OK)
       .json({ message: "Product added successfully", status: "success" });
